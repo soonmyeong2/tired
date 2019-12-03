1129780828a603c0cc7a504248cf193fe9dda8c7
kimyemi <yemi___@khu.ac.kr>
2019-12-03 12:30:52 +0900

    update splitaudioinfo

--- src/aws lambda/splitaudioinfo.py
+++ src/aws lambda/splitaudioinfo.py
@@ -11,7 +11,7 @@ def lambda_handler(event, context):
     key=key_path[-26:-13]
     time=int(key_path[-12:-4])
     time_f=int(time/10000)
-    time_l=time%10000
+    time_l=int(time%10000)
     
     dynamodb=boto3.resource('dynamodb')
     table=dynamodb.Table('tired-sleeptimemerge')
@@ -19,12 +19,21 @@ def lambda_handler(event, context):
     response=table.get_item(Key={"key":key})
     sleeptime=response['Item']['sleeptime']
     splitsleeptime=[]
-    #상대시간 계산. milliseconds
+    splitsleeptime_real=[]
+
     for i in sleeptime:
         i_f=int(i/10000)
-        i_l=i%10000
-        splitsleeptime.extend([(i_f-time_f)*1000,(i_l-time_f)*1000])
-        
+        i_l=int(i%10000)
+        splitsleeptime_real.extend([i_f, i_l])
+
+        #save class period
+        time_t_hour=int(i_f/100)-int(time_f/100)
+        i_f=(i_f-time_f-40*time_t_hour)*10000
+        time_t_hour=int(i_l/100)-int(time_f/100)
+        i_l=(i_l-time_f-40*time_t_hour)*10000
+
+        splitsleeptime.extend([i_f, i_l])
+    
     s3.download_file(bucket, key_path, '/tmp/input_file.mp3')
     sound=AudioSegment.from_mp3('/tmp/input_file.mp3')
     for i in range(0,len(splitsleeptime),2):
